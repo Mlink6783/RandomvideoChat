5f6baf1ca0c8179ffa265a1214e217b64636e9f8
Mlink6783 <mlink6783@gmail.com>
2025-04-20 19:22:35 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,12 +14,19 @@ const io = new Server(server, {
 });
 
 let waitingUser = null;
+let activeUsers = []; // To keep track of all connected users
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  socket.on('join', username => {
+  // Add the new user to the active users list
+  activeUsers.push(socket);
+  console.log("New user connected. Active users: ", activeUsers.length);
+
+  socket.on('join', (username) => {
     socket.username = username;
+
+    // If there is a waiting user, connect both users
     if (waitingUser) {
       const partner = waitingUser;
       waitingUser = null;
@@ -30,6 +37,7 @@ io.on('connection', socket => {
       socket.emit('offer-request');
       partner.emit('offer-request');
     } else {
+      // Otherwise, add the current user to the waiting list
       waitingUser = socket;
     }
   });
@@ -40,19 +48,19 @@ io.on('connection', socket => {
     }
   });
 
-  socket.on('offer', offer => {
+  socket.on('offer', (offer) => {
     if (socket.partner) {
       socket.partner.emit('offer', offer);
     }
   });
 
-  socket.on('answer', answer => {
+  socket.on('answer', (answer) => {
     if (socket.partner) {
       socket.partner.emit('answer', answer);
     }
   });
 
-  socket.on('ice-candidate', candidate => {
+  socket.on('ice-candidate', (candidate) => {
     if (socket.partner) {
       socket.partner.emit('ice-candidate', candidate);
     }
@@ -64,55 +72,49 @@ io.on('connection', socket => {
       socket.partner.partner = null;
     }
     socket.partner = null;
-    if (waitingUser === null) {
-      waitingUser = socket;
-    } else {
-      const partner = waitingUser;
-      waitingUser = null;
-      socket.partner = partner;
-      partner.partner = socket;
+
+    // Try to match with a new random user
+    if (activeUsers.length > 1) {
+      // Remove the current user from the active users list
+      activeUsers = activeUsers.filter(user => user !== socket);
+
+      // Pick a random user from the active users list
+      const randomUser = activeUsers[Math.floor(Math.random() * activeUsers.length)];
+
+      // Match the users
+      socket.partner = randomUser;
+      randomUser.partner = socket;
 
       socket.emit('offer-request');
-      partner.emit('offer-request');
+      randomUser.emit('offer-request');
+    } else {
+      // If no other user is available, add the current user to the waiting list
+      waitingUser = socket;
     }
   });
 
   socket.on('disconnect', () => {
+    // Remove user from active users list
+    activeUsers = activeUsers.filter(user => user !== socket);
+    console.log("User disconnected. Active users: ", activeUsers.length);
+
+    // If the user was waiting, clear waitingUser
     if (waitingUser === socket) waitingUser = null;
+
+    // Disconnect the partner
     if (socket.partner) {
       socket.partner.emit('disconnectPeer');
       socket.partner.partner = null;
     }
   });
-  let activeUsers = [];
 
-io.on('connection', (socket) => {
-    // Add the new user to the active users list
-    activeUsers.push(socket);
-
-    console.log("New user connected. Active users: ", activeUsers.length);
-
-    // Handle "next" button click
-    socket.on('next', () => {
-        if (activeUsers.length > 1) {
-            // Pick a random user from activeUsers list
-            const randomUser = activeUsers[Math.floor(Math.random() * activeUsers.length)];
-
-            // Emit to the current user
-            socket.emit('matched', { userId: randomUser.id });
-
-            // Emit to the random user to notify match
-            randomUser.emit('matched', { userId: socket.id });
-        }
-    });
-
-    // Remove user from activeUsers list on disconnect
-    socket.on('disconnect', () => {
-        activeUsers = activeUsers.filter((user) => user !== socket);
-        console.log("User disconnected. Active users: ", activeUsers.length);
-    });
-});
-
+  // Handle user disconnecting or leaving the call
+  socket.on('disconnectPeer', () => {
+    if (socket.partner) {
+      socket.partner.emit('disconnectPeer');
+      socket.partner.partner = null;
+    }
+  });
 });
 
 server.listen(5000, () => console.log('Server running on port 5000'));
