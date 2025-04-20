b04c37b271985b12d3fe85aba05b6c8e351ae161
Mlink6783 <mlink6783@gmail.com>
2025-04-20 19:35:47 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,19 +41,27 @@ io.on('connection', (socket) => {
   });
 
   socket.on('next', () => {
+    // Remove the current user from the active users list if they have a partner
     if (socket.partner) {
       socket.partner.emit('disconnectPeer');
       socket.partner.partner = null;
     }
+
+    // Disconnect the current user
     socket.partner = null;
 
+    // Ensure there is at least one other user
     if (activeUsers.length > 1) {
+      // Remove this user from the active list
       activeUsers = activeUsers.filter(user => user !== socket);
+
+      // Pick a random user from the remaining active users
       const randomUser = activeUsers[Math.floor(Math.random() * activeUsers.length)];
 
       socket.partner = randomUser;
       randomUser.partner = socket;
 
+      // Emit offer-request to both users
       socket.emit('offer-request');
       randomUser.emit('offer-request');
     } else {
@@ -62,6 +70,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
+    // Remove user from active users list
     activeUsers = activeUsers.filter(user => user !== socket);
     if (waitingUser === socket) waitingUser = null;
 
